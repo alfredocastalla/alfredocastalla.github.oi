9cd5abbdb34c4d9c76dc5be9633b5b34123af667
alfredocastalla <alfredocastalla34@gmail.com>
2024-05-30 14:05:42 +0800

    Update script.js
    Signed-off-by: alfredocastalla <alfredocastalla34@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sampleData = {
-    labels: ['Mon', 'Tue','Wed','Thu', 'fri', 'sat', 'sun'],
+    labels: ['Mon', 'Tue','Wed','Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [{
       label: 'DAYS OF USING GADGETS',
       data: [100, 59, 80, 81, 56, 55, 68, 23, 10, 100, 60, 90],
